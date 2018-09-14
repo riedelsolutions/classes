13edc3fb7a4432ec24ae36717f257379b418cf24
riedelsolutions <38634824+riedelsolutions@users.noreply.github.com>
2018-09-14 05:33:05 -0300

    Add files via upload

---
+++ 01  Variables and Functions Intro/clase01ej.js
@@ -0,0 +1,27 @@
+var name = "Cami";
+var age = 99;
+var food = "Milanesas";
+
+var camiInfo = ["Cami", 99, "Milanesas"];
+
+function withoutArray(){
+	console.log(name + '' + age + '' + food);
+}
+
+function withArray(){
+	console.log(camiInfo);
+}
+
+withoutArray();
+withArray();
+
+
+var camiInfo = ["Cami", 99, "Milanesas"];
+
+
+function withArray(){
+	console.log(camiInfo);
+}
+
+
+withArray();
---
+++ 01  Variables and Functions Intro/funcionesadentrodefuncion.js
@@ -0,0 +1,18 @@
+var a = "ja"
+
+var n = 0;
+
+function name (){
+	console.log(a);
+}
+
+function age(){
+	console.log(n);
+}
+
+function final (){
+	name();
+	age();
+}
+
+final();
