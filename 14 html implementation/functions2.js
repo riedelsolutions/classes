8f6c31417e2b3a52814c6c13cd7bccc36af2a506
Riedel <riedelgcamila@gmail.com>
2019-02-21 23:55:32 -0300

    html implementation

---
+++ 14 html implementation/functions2.js
@@ -0,0 +1,8 @@
+function mouseOverWithClass(){
+	document.getElementsByClassName('paragraphClass')[0].innerHTML = "Pasaste el mouse!";
+	//										change to [1]
+}
+
+function mouseOverWithId(){
+	document.getElementById('paragraphId').innerHTML = "Pasaste el mouse!";
+}
